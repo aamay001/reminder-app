e4cbb37549de9b7cda819a643a3b089981e31231
Andy Amaya <aamay001@gmail.com>
2017-09-16 11:17:57 -0700

    config file typo correction

--- app/config.js
+++ app/config.js
@@ -7,7 +7,7 @@ const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || `mongodb://localhost/
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 const TOKEN_EXP = process.env.TOKEN_EXP || '7d';
 const DEVELOPMENT = process.env.NODE_ENV === 'dev';
-const PRODUCTION = process.env.NODE_END === 'prod';
+const PRODUCTION = process.env.NODE_ENV === 'prod';
 const TWILIO_ACCOUNT = process.env.TWILIO_ACCOUNT;
 const TWILIO_TOKEN = process.env.TWILIO_TOKEN;
 const TWILIO_NUMBER = process.env.TWILIO_NUMBER;
