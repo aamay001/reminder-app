6c58b09c4f1c61a42e2f364621de1e8446f673e0
Andy Amaya <amayaa@Andys-MacBook-Pro.local>
2018-10-03 08:30:33 -0700

    Update cors.

--- controllers/server.js
+++ controllers/server.js
@@ -53,7 +53,7 @@ function stop() {
 }
 
 function cors(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://tellmeondate.herokuapp.com/');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   if (req.method === 'OPTIONS') {
